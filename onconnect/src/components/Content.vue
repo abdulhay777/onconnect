04d836329b3f7b6f74e7208720e1615920c8aa8b
abdulhay <abdulhay.777.2020@gmail.com>
2021-10-28 18:07:55 +0500

    28.10.2021 18:07 Content

--- onconnect/src/components/Content.vue
+++ onconnect/src/components/Content.vue
@@ -1,7 +1,32 @@
 <template>
     <div class="content">
         <div class="content__main-info">
-            
+            <h2>Маъмурий низо - Биринчи инстанция</h2>
+            <div class="content__main-info__ul">
+                <ul>
+                    <li>
+                        <span>Хужжат номери</span>
+                        <p>1234567890</p>
+                    </li>
+                    <li>
+                        <span>Хужжат санаси</span>
+                        <p>12.02.2021</p>
+                    </li>
+                    <li>
+                        <span>Кирим санаси</span>
+                        <p>12.02.2021</p>
+                    </li>
+                    <li>
+                        <span>Жунатиш тури</span>
+                        <p>Почта</p>
+                    </li>
+                     <li>
+                        <span>Манфаатдор идора</span>
+                        <p>Идоранинг номи</p>
+                    </li>
+                </ul>
+                <button>Тулик куриш</button>
+            </div>
         </div>
     </div>
 </template>
