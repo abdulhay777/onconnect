38e31055f66b79bc90779c2aeb80b1cbbb955168
abdulhay777 <75833188+abdulhay777@users.noreply.github.com>
2021-10-28 08:13:55 +0500

    28.10.2021 8:13 header

--- onconnect/src/components/Header.vue
+++ onconnect/src/components/Header.vue
@@ -1,6 +1,30 @@
 <template>
     <div class="header">
-        <img :src="require('../assets/img/header-logo.svg')" >
+        <img :src="require('../assets/img/header-logo.svg')" />
+        <div class="header__right">
+            <div class="header__right__lang">
+                <p>ru</p>
+                <img :src="require('../assets/img/lang.svg')" />
+            </div>
+            <div class="header__right__t header__right__m-16">
+                <img :src="require('../assets/img/T.svg')" />
+            </div>
+            <div class="header__right__full header__right__m-16">
+                <img :src="require('../assets/img/fullscreen.svg')" />
+            </div>
+            <div class="header__right__search header__right__m-16">
+                <img :src="require('../assets/img/search-gray.svg')" />
+            </div>
+            <div class="header__right__avatar header__right__m-16">
+                <div class="header__right__avatar__fullname">
+                    <p>Name</p>
+                    <span>Admin</span>
+                </div>
+                <div class="header__right__avatar__img">
+                    <img />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
